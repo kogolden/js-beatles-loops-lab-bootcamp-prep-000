536986e1418cba66998cf41eaebd343b1dcec124
Katie G <oharagolden@gmail.com>
2018-09-25 18:30:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function johnLennonFacts(facts) {
 }
 function iMehTheBeatles(n) {
   var array = [];
-  var i = 0
+  var i = n + 1
   do   { array.push('I meh the Beatles!');
   i++;
   } while (i < 15);
